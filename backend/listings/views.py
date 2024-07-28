563c4e0c065fea51034b11b8cf854fd595bf7439
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2024-07-28 22:15:15 +0530

    adding listings

--- backend/listings/views.py
+++ backend/listings/views.py
@@ -1,3 +1,129 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView , RetrieveAPIView
+from rest_framework import permissions
+from datetime import datetime , timezone , timedelta
+
+from .models import Listing
+from .serializers import ListingSerializer , ListingDetailSerializer
+
+class ListingsView(ListAPIView):
+    queryset = Listing.objects.order_by('-list_date').filter(is_published = True)
+    permissions_classes = (permissions.AllowAny,)
+    serializer_class = ListingSerializer
+    lookup_field = 'slug'
+
+class ListingView(RetrieveAPIView):
+    queryset = Listing.objects.order_by('-list_date').filter(is_published = True)
+    serializer_class = ListingDetailSerializer
+    lookup_field = 'slug'
+
+class SearchView(APIView):
+    permissions_classes = (permissions.AllowAny,)
+    serializer_class = ListingSerializer
+
+    def post(self, request, format = None):
+        queryset = Listing.objects.order_by('-list_date').filter(is_published = True)
+        data = self.request.data
+
+        sale_type = data['sale_type']
+        queryset = queryset.filter(sale_type__iexact = sale_type)
+
+        price = data['price']
+        if price == '$0+':
+            price = 0
+        elif price == '$100,000+':
+            price = 100000
+        elif price == '$200,000+':
+            price = 200000
+        elif price == '$300,000+':
+            price = 300000
+        elif price == '$400,000+':
+            price = 400000
+        elif price == '$500,000+':
+            price = 500000
+        elif price == '$600,000+':
+            price = 600000
+        elif price == '$800,000+':
+            price = 800000
+        elif price == '$1000,000+':
+            price = 1000000
+        elif price == '$1500,000+':
+            price = 1500000
+        elif price == 'Any':
+            price = -1
+
+        if price != -1:
+            queryset = queryset.filter(price__gte=price)
+
+        bedrooms = data[bedrooms]
+        if bedrooms == '0+':
+            bedrooms = 0
+        elif bedrooms == '1+':
+            bedrooms = 1
+        elif bedrooms == '2+':
+            bedrooms = 2
+        elif bedrooms == '3+':
+            bedrooms = 3
+        elif bedrooms == '4+':
+            bedrooms = 4
+        elif bedrooms == '5+':
+            bedrooms = 5
+
+        queryset = queryset.filter(bedrooms__gte = bedrooms)
+
+        home_type = data[home_type]
+        queryset = queryset.filter(home_type__iexact = home_type)
+
+        bathrooms = data[bathrooms]
+        if bathrooms == '0+':
+            bathrooms = 0.0
+        elif bathrooms == '1+':
+            bathrooms = 1.0
+        elif bathrooms == '2+':
+            bathrooms = 2.0
+        elif bathrooms == '3+':
+            bathrooms = 3.0
+        elif bathrooms == '4+':
+            bathrooms = 4.0
+
+        queryset = queryset.filter(bedrooms__gte = bedrooms)
+        
+        sqft = data[sqft]
+        if sqft == '500+':
+            sqft = 500
+        elif sqft == '1000+':
+            sqft = 1000
+        elif sqft == '1500+':
+            sqft = 1500
+        elif sqft == '2000+':
+            sqft = 2000
+        elif sqft == 'Any':
+            sqft = 0
+
+        if sqft != 0:
+            queryset = queryset.filter(sqft__gte=sqft)
+
+        days_passed = data['days_listed']
+        if days_passed == '1 or less':
+            days_passed = 1
+        elif days_passed == '2 or less':
+            days_passed = 2
+        elif days_passed == '5 or less':
+            days_passed = 5
+        elif days_passed == '10 or less':
+            days_passed = 10
+        elif days_passed == '20 or less':
+            days_passed = 20
+        elif days_passed == 'Any':
+            days_passed = 0
+
+        for query in queryset:
+            num_days = (datetime.now(timezone.utc) - query.list_date).days
+
+            if days_passed != 0:
+                if num_days > days_passed:
+                    slug = query.slug
+                    queryset = queryset.exclude(slug__iexact = slug)
